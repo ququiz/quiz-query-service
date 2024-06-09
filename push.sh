eefb62f739d0a213be4bc3d90302c41985a138f3
lintang-b-s <lintangbirdasaputra23@gmail.com>
2024-06-09 22:37:22 +0700

    reduce timeout auth grpc to 2 second for better experience

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-docker build . -t lintangbirdas/quiz-query-service:18
-docker tag lintangbirdas/quiz-query-service:18 lintangbirdas/quiz-query-service:18
-docker push lintangbirdas/quiz-query-service:18
+docker build . -t lintangbirdas/quiz-query-service:19
+docker tag lintangbirdas/quiz-query-service:19 lintangbirdas/quiz-query-service:19
+docker push lintangbirdas/quiz-query-service:19
 
 
 
